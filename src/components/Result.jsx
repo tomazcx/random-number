51836c69446fb8c6c260d44a9d0bd23080205582
tomazcx <tomazcx06@gmail.com>
2022-06-11 23:53:41 -0300

    Changed result text

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -3,7 +3,7 @@
 export const Result = (props) => {
     return (
         <div className="flex flex-col items-center justify-center">
-            <p className="result w-64 my-6">Entre {props.numbers[0]} e {props.numbers[1]}</p>
+            <p className="result w-64 my-6">Between {props.numbers[0]} and {props.numbers[1]}</p>
             <p className="text-white">The result was:</p>
             <div className="result w-64 mt-6">{props.result}</div>
             <button onClick={props.againFun}>Try again</button>
