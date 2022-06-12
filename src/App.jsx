1b50a80cec34ec406f055a7f17ceae7d5983ffb8
tomazcx <tomazcx06@gmail.com>
2022-06-12 09:58:50 -0300

    Added try catch, remade random number generator algorithm

--- src/App.jsx
+++ src/App.jsx
@@ -9,16 +9,33 @@ export const App = () => {
     const [numbers, setNumbers] = useState([0, 0])
     const [componentActive, setActive] = useState(1)
     const [randomNumber, setRandomNumber] = useState(0)
+    const [errorClass, setError] = useState("hideError")
 
     const generateNum = () => {
-        const diff = Math.abs(numbers[0] - numbers[1])
-        const num = Math.round(Math.random() * diff)
-        if(num <= numbers[0] || num>= numbers[1]){
-           return generateNum()
-        }else{
-            setRandomNumber(num)
-            return setActive(0)
+        try{
+            const num1 = Number(numbers[0])
+            const num2 = Number(numbers[1])
+            const diff = Math.abs(num1 - num2)
+
+            if(diff < 2){
+                throw new Error()
+            }
+
+            const generatedNum = Math.round(Math.random() * diff)
+            const randonNum = generatedNum + num1
+            if(randonNum !== num1 && randonNum !== num2){
+                setRandomNumber(randonNum)
+                setError("hideError")
+                return setActive(0)
+                
+            }else{
+                return generateNum()
+            }
+        }catch(e){
+            setError("showError")
         }
+       
+        
        
     }
 
@@ -34,7 +51,8 @@ export const App = () => {
             ? <Calculate calculateFun={generateNum} setNumbers={setNumbers} numbers={numbers} />
             : <Result againFun={againFun} numbers={numbers} result={randomNumber} />}
             
-            <p className='mt-6 text-white'>Powered by <a href='https://github.com/tomazcx' target="_blank" className='hover:border-b border-solid'>tomazcx</a> </p>
+            <p className={errorClass}>Invalid numbers</p>
+            <p className='mt-6 text-white {}'>Powered by <a href='https://github.com/tomazcx' target="_blank" className='hover:border-b border-solid'>tomazcx</a> </p>
         </main>
     )
 }
